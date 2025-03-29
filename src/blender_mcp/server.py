3d16b828908dcd429b55e98b17ebd7b9b73a3eb9
Andrew Wason <rectalogic@rectalogic.com>
2025-03-29 15:34:58 -0400

    shut down blender on exit

--- src/blender_mcp/server.py
+++ src/blender_mcp/server.py
@@ -1,5 +1,8 @@
 import os
 import subprocess
+import typing as t
+from collections.abc import Generator
+from contextlib import contextmanager
 
 import anyio
 import click
@@ -25,10 +28,21 @@ class BlenderProcess:
                 ],
                 stdin=subprocess.PIPE,
                 stdout=subprocess.PIPE,
-                bufsize=1,
                 text=True,
             )
         return self._process
+
+    def close(self):
+        if self._process is not None:
+            self._process.terminate()
+            try:
+                self._process.wait(5)
+            except subprocess.TimeoutExpired:
+                self._process.kill()
+                try:
+                    self._process.wait(5)
+                except subprocess.TimeoutExpired:
+                    return
 
     def _run_python(self, separator: str, code: str) -> str:
         assert self.process.stdin and self.process.stdout
@@ -47,63 +61,74 @@ class BlenderProcess:
         return self._run_python(">>>exec", code)
 
 
+@contextmanager
+def run_blender(blender_path: str) -> Generator[BlenderProcess, t.Any, None]:
+    process = BlenderProcess(blender_path)
+    try:
+        yield process
+    finally:
+        process.close()
+
+
 async def arun(blender_path: str):
     app = Server("blender-mcp")
-    blender = BlenderProcess(blender_path)
 
-    @app.call_tool()
-    async def run_python(name: str, arguments: dict) -> list[types.TextContent]:
-        match name:
-            case "eval_python":
-                return [
-                    types.TextContent(
-                        type="text", text=blender.eval_python(arguments["expression"])
-                    )
-                ]
-            case "exec_python":
-                return [
-                    types.TextContent(
-                        type="text", text=blender.exec_python(arguments["code"])
-                    )
-                ]
-            case n:
-                raise RuntimeError(f"Invalid tool name {n}")
+    with run_blender(blender_path) as blender:
 
-    @app.list_tools()
-    async def list_tools() -> list[types.Tool]:
-        return [
-            types.Tool(
-                name="eval_python",
-                description="eval a Python expression in Blender. The bpy module is available. The result of the eval is returned. If an exception occurs, the backtrace will be returned.",
-                inputSchema={
-                    "type": "object",
-                    "required": ["expression"],
-                    "properties": {
-                        "expression": {
-                            "type": "string",
-                            "description": "Python expression to eval",
-                        }
+        @app.call_tool()
+        async def run_python(name: str, arguments: dict) -> list[types.TextContent]:
+            match name:
+                case "eval_python":
+                    return [
+                        types.TextContent(
+                            type="text",
+                            text=blender.eval_python(arguments["expression"]),
+                        )
+                    ]
+                case "exec_python":
+                    return [
+                        types.TextContent(
+                            type="text", text=blender.exec_python(arguments["code"])
+                        )
+                    ]
+                case n:
+                    raise RuntimeError(f"Invalid tool name {n}")
+
+        @app.list_tools()
+        async def list_tools() -> list[types.Tool]:
+            return [
+                types.Tool(
+                    name="eval_python",
+                    description="eval a Python expression in Blender. The bpy module is available. The result of the eval is returned. If an exception occurs, the backtrace will be returned.",
+                    inputSchema={
+                        "type": "object",
+                        "required": ["expression"],
+                        "properties": {
+                            "expression": {
+                                "type": "string",
+                                "description": "Python expression to eval",
+                            }
+                        },
                     },
-                },
-            ),
-            types.Tool(
-                name="exec_python",
-                description="exec Python code, possibly multiple lines, in Blender. The bpy module is available. If an exception occurs, the backtrace will be returned.",
-                inputSchema={
-                    "type": "object",
-                    "required": ["code"],
-                    "properties": {
-                        "code": {
-                            "type": "string",
-                            "description": "Python code to exec",
-                        }
+                ),
+                types.Tool(
+                    name="exec_python",
+                    description="exec Python code, possibly multiple lines, in Blender. The bpy module is available. If an exception occurs, the backtrace will be returned.",
+                    inputSchema={
+                        "type": "object",
+                        "required": ["code"],
+                        "properties": {
+                            "code": {
+                                "type": "string",
+                                "description": "Python code to exec",
+                            }
+                        },
                     },
-                },
-            ),
-        ]
+                ),
+            ]
 
-    async with stdio_server() as (read, write):
-        await app.run(read, write, app.create_initialization_options())
+        async with stdio_server() as (read, write):
+            await app.run(read, write, app.create_initialization_options())
 
 
 @click.command()
